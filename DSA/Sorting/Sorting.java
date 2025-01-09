a5366851df61a0862db366224b90d47aded3657f
Jaykant-yadav <jaykant.yadav.cse@gmail.com>
2025-01-09 19:56:52 +0530

    bubble sort

--- DSA/Sorting/Sorting.java
+++ DSA/Sorting/Sorting.java
@@ -1,6 +1,28 @@
-
 public class Sorting {
+    //Print Arrays function
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    //Bubble sort - O(n2)
+    public static void BubbleSort(int arr[]){
+        for(int turn=0; turn<arr.length-1; turn++) {
+            for(int j=0; j<arr.length-1-turn; j++) {
+                //incresing -> (>) & decresing -> (<)
+                if(arr[j] > arr[j+1]) {
+                    //swap
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("");
+        int arr[] = {2, 5, 1, 9, 7, 6};
+        BubbleSort(arr);
+        printArr(arr);
     }
 }
--- DSA/Sorting/sorting.md
+++ DSA/Sorting/sorting.md
@@ -3,8 +3,9 @@
 ![App Screenshot](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWQnX1Pjs2ZvoKcyd0DYx7foMz05jax_3paA&s)
 
 ### Basic Sorting Algorithms <hr>
-- [<b>Bubble Sort</b>](https://github.com/Jaykant-yadav/Data-Structure-Algorithm/blob/main/DSA/Sorting/bubbleSort.java)
+- [<b>Bubble Sort]()
 -> Largest elements come to the end of array by swapping with adjacent elements.
 - [<b>Selection Sort]()
+-> Put the smallest (from unsorted), put it at the beginning
 - [Insertion Sort]()
 - [Counting Sort - Advanced]()
