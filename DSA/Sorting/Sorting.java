8351fc5271fae14beebfd9d23aa717e310d3ad46
Jaykant-yadav <jaykant.yadav.cse@gmail.com>
2025-01-10 14:37:30 +0530

    Insertion sort

--- DSA/Sorting/Sorting.java
+++ DSA/Sorting/Sorting.java
@@ -39,11 +39,28 @@ public class Sorting {
             arr[i] = temp;
         }
     }
+
+    //Insertion sort - O(n2)
+    public static void insertionSort(int arr[]){
+        for(int i=1; i<arr.length; i++) {
+            int curr = arr[i];
+            int prev = i-1;
+            //finding out the correct pos to insert
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            //insertion
+            arr[prev+1] = curr;
+        }
+    }
     public static void main(String[] args) {
         int arr[] = {2, 5, 1, 9, 7, 6};
         // BubbleSort(arr);
         
-        selectionSort(arr);
+        // selectionSort(arr);
+
+        insertionSort(arr);
         printArr(arr);
     }
 }
--- DSA/Sorting/sorting.md
+++ DSA/Sorting/sorting.md
@@ -4,8 +4,9 @@
 
 ### Basic Sorting Algorithms <hr>
 - [<b>Bubble Sort]()
--> Largest elements come to the end of array by swapping with adjacent elements.
+-> Largest elements come to the end of array by swapping with adjacent elements . [video Link]((https://youtu.be/bBQkErahU9c?si=04TTGEh5pF22H-cT))
 - [<b>Selection Sort]()
--> Put the smallest (from unsorted), put it at the beginning
-- [Insertion Sort]()
+-> Put the smallest (from unsorted), put it at the beginning . [video Link](((https://youtu.be/B-nqY6IYqVw?si=VyvdPrXUNU2ML2kY)))
+- [Insertion Sort]() 
+-> Pick an element (from unsorted part) & place in the right pos in sorted part . [video Link]((https://www.youtube.com/watch?v=wWhAhp6PIuQ))
 - [Counting Sort - Advanced]()
