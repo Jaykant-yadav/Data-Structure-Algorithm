b7468002c9d70d5ca6cffd769f34adc0b285922b
Jaykant-yadav <jaykant.yadav.cse@gmail.com>
2025-01-12 16:12:52 +0530

    PlusOne

--- DSA/Arrays/binarySearch.java
+++ DSA/Arrays/binarySearch.java
@@ -1,7 +1,8 @@
 package DSA.Arrays;
 
 public class binarySearch {
-    //Time Complexity - O(log n)
+    //Time Complexity - O(log n) -> Binary Search always apply on Sorted
+    //TC-> N->N/2->N/4->N/8----[1]
     public static int search(int num[], int target) {
         int start = 0, end = num.length-1;
         while (start <= end) {
