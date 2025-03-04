f3b96f34cde21930e4a217129ea04160f92bbb09
Jaykant-yadav <jaykant.yadav.cse@gmail.com>
2025-03-04 12:21:31 +0530

    Quick Sort

--- DSA/divideAndConquer/divideConquer.java
+++ DSA/divideAndConquer/divideConquer.java
@@ -3,45 +3,45 @@ package DSA.divideAndConquer;
 public class divideConquer {
 
     public static void printArr(int arr[]) {
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
 
-    //Merge Sort
-    public static void mergeSort(int arr[], int si, int ei){
-        //Base Case
-        if(si >= ei) {
+    // Merge Sort
+    public static void mergeSort(int arr[], int si, int ei) {
+        // Base Case
+        if (si >= ei) {
             return;
         }
 
-        //Work
-        int mid = si + (ei - si) / 2; //(si+ei)/2
-        mergeSort(arr, si, mid); //left part
-        mergeSort(arr, mid+1, ei); //right part
-        merge(arr, si, mid, ei); //Merge left & right
+        // Work
+        int mid = si + (ei - si) / 2; // (si+ei)/2
+        mergeSort(arr, si, mid); // left part
+        mergeSort(arr, mid + 1, ei); // right part
+        merge(arr, si, mid, ei); // Merge left & right
     }
 
     public static void merge(int arr[], int si, int mid, int ei) {
-        //Temp Array
-        //left(0, 3) right(4, 6) = 3 -> 6 - 0 -> 6 + 1 => 7
-        int temp[] = new int[ei-si+1];
-        int i = si; //iterator for left part
-        int j = mid+1; //iterator for right part
-        int k = 0; //iterator for temp arr
+        // Temp Array
+        // left(0, 3) right(4, 6) = 3 -> 6 - 0 -> 6 + 1 => 7
+        int temp[] = new int[ei - si + 1];
+        int i = si; // iterator for left part
+        int j = mid + 1; // iterator for right part
+        int k = 0; // iterator for temp arr
         while (i <= mid && j <= ei) {
-            if(arr[i] < arr[j]) {
+            if (arr[i] < arr[j]) {
                 temp[k] = arr[i];
                 i++;
-            }else {
+            } else {
                 temp[k] = arr[j];
                 j++;
             }
             k++;
         }
 
-        //left part
+        // left part
         while (i <= mid) {
             temp[k++] = arr[i++];
         }
@@ -51,25 +51,49 @@ public class divideConquer {
             temp[k++] = arr[j++];
         }
 
-        //copy temp to original arr
-        for(k=0, i=si; k<temp.length; k++, i++){
+        // copy temp to original arr
+        for (k = 0, i = si; k < temp.length; k++, i++) {
             arr[i] = temp[k];
         }
     }
 
-    //Quick Sort
+    // Quick Sort
     public static void quickSort(int arr[], int si, int ei) {
-        //last element
-
-    }
-    public static void main(String[] args) {
-        int arr[] = {6, 3, 9, 5, 2, 8, -4};
-        mergeSort(arr, 0, arr.length-1);
-        quickSort(arr, 0, arr.length-1);
-        // printArr(arr);
-        int n = 17;
-        for(int i=1; i<=10; i++) {
-            System.out.println(i*n);
+        //base case
+        if(si >= ei) {
+            return;
         }
+        // last element
+        int pIdx = partition(arr, si, ei);
+        quickSort(arr, si, pIdx - 1); // left
+        quickSort(arr, pIdx + 1, ei); // right
+    }
+
+    public static int partition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+        int i = si - 1; // to make palce for elements smaller than pivot
+
+        for (int j = si; j < ei; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        // swap
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 9, 5, 2, 8};
+        // mergeSort(arr, 0, arr.length - 1);
+        quickSort(arr, 0, arr.length - 1);
+        printArr(arr);
     }
 }
